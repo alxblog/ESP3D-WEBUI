3f7405016263352a6c3f6e2dfe8d7937c4618b63
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-15 11:36:54 +0100

    Update preact-cli config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import { h, render } from "preact"
+// import "regenerator-runtime/runtime.js"
+// import { h, render } from "preact"
 import { App } from "./components/App"
 import './style/index.scss';
-render(
-        <App />,
-    document.body
-)
+// render(
+//         <App />,
+//     document.body
+// )
+
+export default App;
