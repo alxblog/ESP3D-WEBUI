4618969b9a1a9129dadc7447d91df563b544f724
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-22 20:12:18 +0100

    Remove useless line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { h, render } from "preact"
 import "preact/debug"; //temp preact devtool integration
-const root = document.getElementById('root')
 
 import { App } from "./components/App"
 import './style/index.scss';
