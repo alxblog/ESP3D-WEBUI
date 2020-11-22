a6ce877d28e08066e9b31e3462eb2bfb88fec8e9
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-22 15:01:25 +0100

    Create Spectre utils componants

---
+++ src/components/Spectre/index.js
@@ -0,0 +1,7 @@
+import Tile from './Tile'
+import Button from './Button'
+import Panel from './Panel'
+import Table from './Table'
+// const Spectre = { Tile }
+// export default Spectre
+export { Tile, Button, Panel, Table }
