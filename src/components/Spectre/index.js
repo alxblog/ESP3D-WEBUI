85bc3a700eddd5ef46e2acd42dff5cea626215f6
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-24 22:33:47 +0100

    Update and Add Spectre.ccs components

--- src/components/Spectre/index.js
+++ src/components/Spectre/index.js
@@ -1,7 +1,24 @@
-import Tile from './Tile'
 import Button from './Button'
+import Divider from './Divider'
+import Loading from './Loading'
+import Menu from './Menu'
+import Modal from './Modal'
 import Panel from './Panel'
+import Progress from './Progress'
 import Table from './Table'
+import Tile from './Tile'
+import Toast from './Toast'
 // const Spectre = { Tile }
 // export default Spectre
-export { Tile, Button, Panel, Table }
+export {
+    Button,
+    Panel,
+    Progress,
+    Table,
+    Tile,
+    Toast,
+    Loading,
+    Modal,
+    Menu,
+    Divider
+}
